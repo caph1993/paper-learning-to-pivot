1f9fe2e0eff432307b15b9d200868ff64f17881c
Carlos Pinzón <caph1993@gmail.com>
2019-11-13 23:42:03 +0100

    fixed jets.py deprecation issues

--- code/jets.py
+++ code/jets.py
@@ -31,25 +31,25 @@ Dx = Dense(64, activation="tanh")(inputs)
 Dx = Dense(64, activation="relu")(Dx)
 Dx = Dense(64, activation="relu")(Dx)
 Dx = Dense(1, activation="sigmoid")(Dx)
-D = Model(input=[inputs], output=[Dx])
+D = Model(inputs=[inputs], outputs=[Dx])
 
 Rx = D(inputs)
 Rx = Dense(64, activation="relu")(Rx)
 Rx = Dense(64, activation="relu")(Rx)
 Rx = Dense(64, activation="relu")(Rx)
 Rx = Dense(z.shape[1], activation="softmax")(Rx)
-R = Model(input=[inputs], output=[Rx])
+R = Model(inputs=[inputs], outputs=[Rx])
 
 
 def make_loss_D(c):
     def loss_D(y_true, y_pred):
-        return c * K.binary_crossentropy(y_pred, y_true)
+        return c * K.binary_crossentropy(y_true, y_pred)
     return loss_D
 
 
 def make_loss_R(c):
     def loss_R(z_true, z_pred):
-        return c * K.categorical_crossentropy(z_pred, z_true)
+        return c * K.categorical_crossentropy(z_true, z_pred)
     return loss_R
 
 
@@ -57,27 +57,27 @@ opt_D = Adam()
 D.compile(loss=[make_loss_D(c=1.0)], optimizer=opt_D)
 
 opt_DRf = SGD(momentum=0)
-DRf = Model(input=[inputs], output=[D(inputs), R(inputs)])
+DRf = Model(inputs=[inputs], outputs=[D(inputs), R(inputs)])
 DRf.compile(loss=[make_loss_D(c=1.0),
                   make_loss_R(c=-lam)],
             optimizer=opt_DRf)
 
 opt_DfR = SGD(momentum=0)
-DfR = Model(input=[inputs], output=[R(inputs)])
+DfR = Model(inputs=[inputs], outputs=[R(inputs)])
 DfR.compile(loss=[make_loss_R(c=1.0)],
             optimizer=opt_DfR)
 
 # Pretraining of D
 D.trainable = True
 R.trainable = False
-D.fit(X_train, y_train, nb_epoch=20)
+D.fit(X_train, y_train, epochs=20)
 
 # Pretraining of R
 if lam > 0.0:
     D.trainable = False
     R.trainable = True
-    DfR.fit(X_train[y_train == 0], z_train[y_train == 0], nb_epoch=20)
-    # DfR.fit(X_train, z_train, nb_epoch=20)
+    DfR.fit(X_train[y_train == 0], z_train[y_train == 0], epochs=20)
+    # DfR.fit(X_train, z_train, epochs=20)
 
 # Adversarial training
 batch_size = 128
@@ -97,8 +97,8 @@ for i in range(1001):
         R.trainable = True
 
         DfR.fit(X_train[y_train == 0], z_train[y_train == 0],
-                batch_size=batch_size, nb_epoch=1, verbose=0)
+                batch_size=batch_size, epochs=1, verbose=0)
         # DfR.fit(X_train, z_train,
-        #         batch_size=batch_size, nb_epoch=1, verbose=0)
+        #         batch_size=batch_size, epochs=1, verbose=0)
 
 D.save_weights("D-%.4f-%d-z=0.h5" % (lam, seed))
